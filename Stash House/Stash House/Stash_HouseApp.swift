143ee7f8578c7d799b8831564f96f472409d3335
j.TRUbela <81325704+jtrubela@users.noreply.github.com>
2023-06-30 23:24:21 -0400

    Add files via upload

---
+++ Stash House/Stash House/Stash_HouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stash_HouseApp.swift
+//  Stash House
+//
+//  Created by Justin Trubela on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Stash_HouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
